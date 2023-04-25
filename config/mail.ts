4835542455c7d72bd2a1b994418344f269ece52d
JaredLoera <109704913+JaredLoera@users.noreply.github.com>
2023-04-25 10:37:50 -0600

    alex servers dos

---
+++ config/mail.ts
@@ -0,0 +1,55 @@
+/**
+ * Config source: https://git.io/JvgAf
+ *
+ * Feel free to let us know via PR, if you find something broken in this contract
+ * file.
+ */
+
+import { mailConfig } from '@adonisjs/mail/build/config'
+
+export default mailConfig({
+  /*
+  |--------------------------------------------------------------------------
+  | Default mailer
+  |--------------------------------------------------------------------------
+  |
+  | The following mailer will be used to send emails, when you don't specify
+  | a mailer
+  |
+  */
+  mailer: 'smtp',
+
+  /*
+  |--------------------------------------------------------------------------
+  | Mailers
+  |--------------------------------------------------------------------------
+  |
+  | You can define or more mailers to send emails from your application. A
+  | single `driver` can be used to define multiple mailers with different
+  | config.
+  |
+  | For example: Postmark driver can be used to have different mailers for
+  | sending transactional and promotional emails
+  |
+  */
+  mailers: {
+    /*
+    |--------------------------------------------------------------------------
+    | Smtp
+    |--------------------------------------------------------------------------
+    |
+    | Uses SMTP protocol for sending email
+    |
+    */
+    smtp: {
+      driver: 'smtp',
+      host: 'smtp.mailgun.org',
+      port: '587',
+      auth: {
+        user: 'postmaster@sandboxd81b8e1bc71346d0bdccf4ce7e87d4d4.mailgun.org',
+        pass: '9481d7994cc2274c8e92a5409c872342-48c092ba-852abe29',
+        type: 'login',
+      },
+    },
+  },
+})
